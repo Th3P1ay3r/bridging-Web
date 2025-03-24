de4431ef112c899b9ae0280ccc6609894af5b3d0
DeadPoolTheFoolOne <deadpoolthefoolone@gmail.com>
2025-03-24 14:16:17 +0630

    add list

--- week1/chapter4/pizza4_11.py
+++ week1/chapter4/pizza4_11.py
@@ -1,3 +1,6 @@
+
+my_foods = ['pizza','falafel','carrot', 'cake']#source
+friend_foods=my_foods[:]#start zero through the last item
 my_foods.append('pepparoni pizza')
 friend_foods.append('prawn pizza')
 
